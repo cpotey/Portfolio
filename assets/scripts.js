c8e15cffdd985d6ca3394c6a73ff58c73931a7e7
Connor Pote <cpotey@me.com>
2018-12-28 14:31:21 +0000

    Initial commit

---
+++ assets/scripts.js
@@ -0,0 +1,11 @@
+// var imageArray = [
+//   "",
+//   "",
+// ];
+
+// var size = imageArray.length;
+// var x = Math.floor(size*Math.random());
+
+// console.log(imageArray[x]);
+
+// document.querySelector('.header-container').style.backgroundImage = "url(" + imageArray[x] + ")";
