653fce51134a4b67825b72fd3152d9edc99875fd
Connor Pote <cpotey@me.com>
2019-03-03 22:30:06 +0000

    Updates to site

--- assets/scripts.js
+++ assets/scripts.js
@@ -9,3 +9,23 @@
 // console.log(imageArray[x]);
 
 // document.querySelector('.header-container').style.backgroundImage = "url(" + imageArray[x] + ")";
+
+//   LazyLoad bg_images
+// document.addEventListener("DOMContentLoaded", function() {
+//     var lazyBackgrounds = [].slice.call(document.querySelectorAll(".lazy-background"));
+  
+//     if ("IntersectionObserver" in window) {
+//       let lazyBackgroundObserver = new IntersectionObserver(function(entries, observer) {
+//         entries.forEach(function(entry) {
+//           if (entry.isIntersecting) {
+//             entry.target.classList.add("visible");
+//             lazyBackgroundObserver.unobserve(entry.target);
+//           }
+//         });
+//       });
+  
+//       lazyBackgrounds.forEach(function(lazyBackground) {
+//         lazyBackgroundObserver.observe(lazyBackground);
+//       });
+//     }
+//   });
